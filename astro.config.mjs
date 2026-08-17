d26dce1d7ee9d4a0c982c7335599dcdd6b5465a8
Axel Refalo <axeL.refalo@gmail.com>
2026-08-17 14:25:01 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://axelrefalo.github.io',
   redirects: {
-    '/': '/portofolio',
+    '/portofolio': '/',
   },
   markdown: {
     remarkPlugins: [remarkMath],
